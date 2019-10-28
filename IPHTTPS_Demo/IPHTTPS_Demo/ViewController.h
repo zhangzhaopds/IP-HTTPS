7ff51ebd6bdb775e75c275549f44281743e95e96
张昭 <zhangzhaopds@foxmail.com>
2019-10-28 22:11:10 +0800

    first commit

---
+++ IPHTTPS_Demo/IPHTTPS_Demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IPHTTPS_Demo
+//
+//  Created by 张昭 on 2019/10/28.
+//  Copyright © 2019 HeyFox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ IPHTTPS_Demo/IPHTTPS_Demo/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  IPHTTPS_Demo
+//
+//  Created by 张昭 on 2019/10/28.
+//  Copyright © 2019 HeyFox. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
