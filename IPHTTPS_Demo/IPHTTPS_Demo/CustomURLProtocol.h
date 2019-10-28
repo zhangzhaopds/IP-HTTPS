7e5c75523d98d1ede1bb0ef2d4debf7e03f1247c
张昭 <zhangzhaopds@foxmail.com>
2019-10-28 23:02:13 +0800

    Add NetworkManager

---
+++ IPHTTPS_Demo/IPHTTPS_Demo/CustomURLProtocol.h
@@ -0,0 +1,17 @@
+//
+//  CustomURLProtocol.h
+//  IPHTTPS_Demo
+//
+//  Created by 张昭 on 2019/10/28.
+//  Copyright © 2019 HeyFox. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CustomURLProtocol : NSURLProtocol
+
+@end
+
+NS_ASSUME_NONNULL_END
